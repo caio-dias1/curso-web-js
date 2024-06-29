7b37ff1f1a415613b82fb228ed20a50d65b3ea17
Caio Dias <caior.dias11@gmail.com>
2024-06-29 12:13:27 -0300

    Create objeto.js

---
+++ exercicios-js/objeto.js
@@ -0,0 +1,15 @@
+const prod1 = {}
+prod1.nome = 'Celular X'
+prod1.preco = 4882.90
+
+console.log(prod1)
+
+
+const prod2 = {
+    nome: 'Camisa Nike',
+    preco: 89.90
+}
+
+'{"nome": "Camisa Nike","preco": 89.90}' // JSON (UM FORMATO TEXTUAL DE UM OBJETO)
+
+console.log(prod2)
