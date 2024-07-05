dc025168b46a47b6be4523384e34557c8a8bd0b2
Caio Dias <caior.dias11@gmail.com>
2024-07-04 21:45:36 -0300

    destructuring

---
+++ exercicios-js/fundamentos/destructuring1.js
@@ -0,0 +1,11 @@
+const pessoa = {
+    nome: 'caio',
+    idade: 24,
+    endereco: {
+        rua: 'jose sponchiado',
+        numero: 200
+    }
+}
+
+const { nome, idade } = pessoa
+console.log(nome, idade)
---
+++ exercicios-js/fundamentos/destructuring2.js
@@ -0,0 +1,5 @@
+const [a] = [10]
+console.log(a)
+
+const [n1,  , n3, n5, , n7 = 0] = [10, 8, 4, 5]
+console.log(n1, n3, n5, n7)
