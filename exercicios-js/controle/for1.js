8302f683d5928fe6d7a703936cc8f98a8369eab5
Caio Dias <caior.dias11@gmail.com>
2024-07-10 19:51:39 -0300

    Create for1.js

---
+++ exercicios-js/controle/for1.js
@@ -0,0 +1,14 @@
+let contador = 1
+while(contador <= 10) {
+    console.log(`contador = ${contador}`)
+    contador++
+}
+
+for(let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+
+const notas = [6.7, 5.4, 8.9, 9.1, 4.1]
+for(let i = 0; i < notas.length; i++) {
+    console.log(`notas = ${notas[i]}`)
+}
