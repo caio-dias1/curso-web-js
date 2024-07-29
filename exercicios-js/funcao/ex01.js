bf954a0746dbd915bf3a32a874cdb7c42e2eacfb
Caio Dias <caior.dias11@gmail.com>
2024-07-29 19:41:32 -0300

    Create ex01.js

---
+++ exercicios-js/funcao/ex01.js
@@ -0,0 +1,12 @@
+function operacoesMatematicas(x, y) {
+    let soma = x + y
+    let sub = x - y
+    let mult = x * y
+    let div = x / y
+    console.log(`O resultado da soma de ${x} + ${y} = ${soma}`)
+    console.log(`O resultado da soma de ${x} - ${y} = ${sub}`)
+    console.log(`O resultado da soma de ${x} x ${y} = ${mult}`)
+    console.log(`O resultado da soma de ${x} : ${y} = ${div}`)
+}
+
+operacoesMatematicas(4, 4)
