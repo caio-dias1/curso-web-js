20e93c00e57190b2528b9e77f19023b1fdb3c045
Caio Dias <caior.dias11@gmail.com>
2024-08-05 19:33:01 -0300

    Create oo.js

---
+++ objetos/oo.js
@@ -0,0 +1,20 @@
+// Procedural
+processamento(valor,1 valor2, valor3) // uma funcao que manipula dados
+
+// OO = Dados e dentro desses dados tem funcoes
+objeto = {
+    valor,
+    valor2,
+    valor3,
+    processamento() {
+        // ....
+    }
+}
+
+objeto.processamento() // o ojbeto funciona como uma capsula que agrupa comportamentos e atributos, o foco passou a ser o objeto
+
+// principios importantes
+// 1 - abstraçao (pegar um objeto do mundo real e traduzir para seu sistema)
+// 2 - encapsulamento (esconder os detalhes de implementaçao, visivel apenas o necessario para interface de comunicaçao(usuario))
+// 3 - herança (receber atributos e comportamentos de um objeto pai)
+// 4 - polimorfismo (multipla formas. Usar um conceito generico e poder atribuir subtipos a essa variavel)
