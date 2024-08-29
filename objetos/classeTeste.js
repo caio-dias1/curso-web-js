fa80bbf3edad86515f7dc11486085d759dbe4ec6
Caio Dias <caior.dias11@gmail.com>
2024-08-29 20:56:39 -0300

    Update classeTeste.js

--- objetos/classeTeste.js
+++ objetos/classeTeste.js
@@ -1,18 +1,17 @@
-class Carro {
-    constructor(marca, preco) {
+class Mercedes {
+    constructor(marca = 'Mercedes') {
         this.marca = marca
-        this.preco = preco
     }
 }
 
-class Mercedes extends Carro {
-    constructor(marca, preco, modelo) {
-        this.marca = 'mercedes'
-        this.preco = this.preco
+class C63 extends Mercedes {
+    constructor(marca, preco = 180000 , modelo = 'C63 AMG') {
+        super(marca)
+        this.preco = preco
         this.modelo = modelo
     }
 
 }
 
-const C63 = new Mercedes(180000, 'C63')
-console.log(C63)
+const C63AMG = new C63
+console.log(C63AMG)
