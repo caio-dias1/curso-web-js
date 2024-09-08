12ade9af39d03f51f680369e7a5fdbe7a4f3bdcf
Caio Dias <caior.dias11@gmail.com>
2024-09-08 20:13:42 -0300

    Create map02.js

---
+++ exercicios-js/array/map02.js
@@ -0,0 +1,22 @@
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45}',
+    '{"nome": "Caderno", "preco": 13.90}',
+    '{"nome": "Kit de Lapis", "preco": 41.22}',
+    '{"nome": "Caneta", "preco": 7.50}'
+]
+
+/* retornar um array apenas com os preços
+
+const valores = carrinho.map(e => {
+    const produto = JSON.parse(e)
+    return produto.preco
+}) */
+
+console.log(valores)
+
+// metodo do professor
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
